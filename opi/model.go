559c98ef251dcf99332626244cdb6a5e7af94464
Nima Kaviani <nkavian@us.ibm.com>
2019-04-09 11:05:47 -0700

    implement secure staging
    - break staging into three containers for download, build, upload
    - mount certs dir, download appbits and buildpacks
    - mount dir for appbits and buildpacks, run buildpack, put droplet in
    output dir
    - mount certs dir and output dir, upload droplet
    
    [#163723137](https://www.pivotaltracker.com/story/show/163723137)
    
    Signed-off-by: Matthew Broomfield <mattysweeps@google.com>

--- opi/model.go
+++ opi/model.go
@@ -71,6 +71,13 @@ type Task struct {
 	Env     map[string]string
 }
 
+type StagingTask struct {
+	*Task
+	DownloaderImage string
+	UploaderImage   string
+	ExecutorImage   string
+}
+
 //go:generate counterfeiter . Desirer
 type Desirer interface {
 	Desire(lrp *LRP) error
@@ -84,6 +91,6 @@ type Desirer interface {
 //go:generate counterfeiter . TaskDesirer
 type TaskDesirer interface {
 	Desire(task *Task) error
-	DesireStaging(task *Task) error
+	DesireStaging(task *StagingTask) error
 	Delete(name string) error
 }
--- recipe/unzip_test.go
+++ recipe/unzip_test.go
@@ -128,6 +128,11 @@ var _ = Describe("Unzip function", func() {
 				BeforeEach(func() {
 					srcZip = "testdata/just_files.zip"
 				})
+
+				AfterEach(func() {
+					cleanUpFiles()
+				})
+
 				assertFilesUnzippedSuccessfully()
 			})
 
@@ -135,6 +140,11 @@ var _ = Describe("Unzip function", func() {
 				BeforeEach(func() {
 					srcZip = "testdata/unzip_me.zip"
 				})
+
+				AfterEach(func() {
+					cleanUpFiles()
+				})
+
 				assertFilesUnzippedSuccessfully()
 			})
 		})
